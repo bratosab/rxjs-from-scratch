33e4fd41aa315e5e92fa848c0d0ceb91b107bf4a
Abdallah Bratos <bratosab@gmail.com>
2021-06-07 10:55:52 +0200

    feat(observable)

---
+++ src/1-observable.js
@@ -0,0 +1,46 @@
+import { Observable, of, interval, fromEvent, from } from "rxjs";
+
+/**
+ * Basic observable
+ */
+const observable = Observable.create((observer) => {
+  observer.next("Bonjour !");
+  observer.next("Bonsoir !");
+});
+
+// observable.subscribe((val) => console.log(val));
+
+/**
+ * From DOM event
+ */
+const $click = fromEvent(document, "click");
+// $click.subscribe(console.log);
+
+/**
+ * Simulate async opération (API call), convert to Observable
+ */
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("DONE !");
+  }, 1000);
+});
+
+const $obsFromPromise = from(promise);
+// $obsFromPromise.subscribe((val) => console.log(val));
+
+/**
+ * Periodic subscription
+ */
+const $periodic = interval(1000);
+// $periodic.subscribe(console.log);
+
+/**
+ * Pass static values to Observables
+ */
+const $string = of("hello");
+const $anything = of("hello", ["an", "array"], true, {
+  id: 1,
+  name: "best beer",
+});
+//  $string.subscribe(console.log);
+//  $anything.subscribe(console.log);
