19154b2ed74b7ba2cda64e34a9e81f084715a77a
Abdallah Bratos <bratosab@gmail.com>
2021-06-07 12:06:29 +0200

    feat(catch)

---
+++ src/8-catch.js
@@ -0,0 +1,51 @@
+import { Observable, of, throwError } from 'rxjs'
+import { catchError, retry, map } from 'rxjs/operators'
+
+/**
+ * Basic observable
+ */
+const observable$ = Observable.create((observer) => {
+  observer.next('Bonjour !')
+  observer.next('Bonsoir !')
+  observer.next('Bonne nuit !')
+  throw "J'ai pas envie de dormir !"
+  observer.next('Bien dormi ?')
+})
+
+/**
+ * Catch Error
+ */
+observable$.pipe(
+  catchError((err) => {
+    console.error(err)
+    return of(null)
+  })
+)
+// .subscribe(console.log)
+
+/**
+ * Retry
+ */
+observable$.pipe(
+  catchError((err) => {
+    console.error(err)
+    return of(null)
+  }),
+  retry(2)
+)
+// .subscribe(console.log)
+
+/**
+ * Control error in subscription
+ */
+observable$.pipe(
+  catchError((err) => {
+    console.error(err)
+    return throwError('it broke')
+  }),
+  retry(2)
+)
+// .subscribe({
+//   next: (val) => console.log(val),
+//   error: (val) => console.log('error', val),
+// })
